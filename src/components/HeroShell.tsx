76d5e7003d60a73c0568be89780023ba531d4291
terryszhou <terryszhou@gmail.com>
2022-03-14 10:08:06 -0500

    HeroShell and useOnScreen hook added

---
+++ src/components/HeroShell.tsx
@@ -0,0 +1,84 @@
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+  useColorModeValue as uCMV, 
+} from '@chakra-ui/react';
+import * as React from "react";
+
+interface HeroShellProps {
+  children?: any,
+  label?: string,
+  loaded?: boolean | React.Dispatch<any> | React.MutableRefObject<any>,
+};
+
+export const HeroShell = ({ children, label, loaded }: HeroShellProps) => {
+  const number: string = label?.split(".")[0] + "."
+  const title: string = " " + label?.split(".")[1]
+
+  return (
+    <Container
+      maxW={{ base: "95%", sm: "85%", lg: "80%", xl: "70%" }}
+      opacity={loaded ? 1 : 0}
+      transition={"500ms ease-out"}>
+      <Stack
+        align={'center'}
+        direction={{ base: 'column', md: 'row' }}
+        paddingY={18}
+        paddingTop={"7.5%"}
+        spacing={{ base: 8, md: 10 }}>
+        <Stack flex={2} spacing={{ base: 5, md: 10 }} position={"relative"}>
+          <Stack position={"relative"} spacing={3}>
+            <Box 
+              backgroundColor={"goldenrod"}
+              boxShadow={uCMV("none","0 0 5px goldenrod")}
+              height={2}
+              opacity={0}
+              position={"absolute"}
+              top={"2%"}
+              width={0}
+              _after={{
+                borderTop: "8px solid goldenrod",
+                borderRight: "8px solid transparent",
+                content: `""`,
+                filter: uCMV("none","drop-shadow(2px 0 5px goldenrod)"),
+                position: "absolute",
+                right: -2,
+                width: 0 }} />
+              <Heading
+                fontFamily={"var(--chakra-fonts-mono)"}
+                fontSize={{ base: 'lg', sm: 'xl', md: "2xl" }}
+                fontWeight={"bold"}>
+                <Text 
+                  as={"span"}
+                  color={"goldenrod"}
+                  textShadow={uCMV("none","0 0 5px goldenrod")}>
+                  {number}
+                </Text>{title}
+              </Heading>
+          </Stack>
+          <Flex
+            align={'center'}
+            bottom={3}
+            flex={1}
+            justify={'center'}
+            position={'relative'}
+            w={'full'}>
+            {children[0]}
+          </Flex>
+        </Stack>
+        <Flex
+          align={'center'}
+          flex={0}
+          justify={'center'}
+          position={'relative'}
+          w={'full'}>
+          {children[1]}
+        </Flex>
+      </Stack>
+    </Container>
+  );
+};
