f7228110589399b8afb75576266c2f3e8338bae4
terryszhou <terryszhou@gmail.com>
2022-03-14 19:55:54 -0500

    React icons added, additional stubbing for aboutNLTK

--- src/components/AboutNLTK.tsx
+++ src/components/AboutNLTK.tsx
@@ -1,5 +1,7 @@
-import { Image, Box, Flex, Stack, Text } from "@chakra-ui/react";
+import { Image, Box, Flex, Stack, Text, List, ListItem, ListIcon, useColorModeValue as colorMode } from "@chakra-ui/react";
 import * as React from "react";
+
+import { BsFillSuitHeartFill } from "react-icons/bs";
 
 import { HeroShell } from "./HeroShell";
 import { PageProps } from "../helpers/interfaces";
@@ -20,12 +22,50 @@ export const AboutNLTK = ({ visRef, visible }: PageProps) => {
           <Text>
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
           </Text>
+          <List>
+            <ListItem
+              color={colorMode("rgba(44,44,44,0.8)","rgba(180,180,180,0.8)")}
+              display={"flex"}
+              fontFamily={"var(--chakra-fonts-nunito)"}
+              fontSize={"lg"}
+              marginY={2}>
+              <ListIcon
+                as={BsFillSuitHeartFill}
+                color='green.500'
+                marginTop={1} />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            </ListItem>
+            <ListItem
+              color={colorMode("rgba(44,44,44,0.8)","rgba(180,180,180,0.8)")}
+              display={"flex"}
+              fontFamily={"var(--chakra-fonts-nunito)"}
+              fontSize={"lg"}
+              marginY={2}>
+              <ListIcon
+                as={BsFillSuitHeartFill}
+                color='green.500'
+                marginTop={1} />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            </ListItem>
+            <ListItem
+              color={colorMode("rgba(44,44,44,0.8)","rgba(180,180,180,0.8)")}
+              display={"flex"}
+              fontFamily={"var(--chakra-fonts-nunito)"}
+              marginY={2}>
+              <ListIcon
+                as={BsFillSuitHeartFill}
+                color='green.500'
+                marginTop={1} />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            </ListItem>
+          </List>
         </Stack>
       </Box>
       <Flex
         alignItems={"center"}
         justifyContent={"center"}
         flexDirection={"column"}
+        marginBottom={"10%"}
         marginTop={"10%"}>
         <Box
           alignItems={"center"}
