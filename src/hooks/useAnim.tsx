b636fd08ab21ac8bc347bb32218cbff0fa2b032c
terryszhou <terryszhou@gmail.com>
2022-03-23 10:38:53 -0500

    Remove unnecessary imports for netlify deploy

--- src/hooks/useAnim.tsx
+++ src/hooks/useAnim.tsx
@@ -1,5 +1,4 @@
 import { usePrefersReducedMotion } from "@chakra-ui/react";
-import * as React from "react";
 
 export const useAnim = (animation: string): undefined | string => {
   const prefersReducedMotion: boolean = usePrefersReducedMotion();
